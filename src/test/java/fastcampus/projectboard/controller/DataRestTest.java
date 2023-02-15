c9a62113c3da394be1ca6c7f530ae0f4a8d15628
mdgome <mdgome3@gmail.com>
2023-02-15 16:58:16 +0900

    `#1 - Spring DATA REST 통합 테스트 제외 처리 비즈니스 로직이 아니며, 단순 Data Rest 기능 확인 용도이며 통합 테스트로 무거워 제외 처리 진행 공부 목적으로 Ref를 남기기 위하여 삭제는 하지 않음.

--- src/test/java/fastcampus/projectboard/controller/DataRestTest.java
+++ src/test/java/fastcampus/projectboard/controller/DataRestTest.java
@@ -1,5 +1,6 @@
 package fastcampus.projectboard.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
+@Disabled("Spring Data REST 통합 테스트 제외")
 @DisplayName("Data Rest - API 테스트")
 @Transactional
 @AutoConfigureMockMvc
