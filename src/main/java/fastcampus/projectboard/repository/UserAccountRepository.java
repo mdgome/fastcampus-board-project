bfa1cbfbaf00012412cab4ca4c611a65c8846b2b
mdgome <mdgome3@gmail.com>
2023-03-10 23:43:29 +0900

    `#4 - 회원 계정 엔티티 추가 계정 엔티티 관련 Dto, 도메인, repository 추가

---
+++ src/main/java/fastcampus/projectboard/repository/UserAccountRepository.java
@@ -0,0 +1,9 @@
+package fastcampus.projectboard.repository;
+
+import fastcampus.projectboard.domain.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface UserAccountRepository extends JpaRepository<UserAccount, String>{
+}
